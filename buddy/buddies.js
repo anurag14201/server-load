1c2b5598a61032c7bed1bb37adfc234454f8726b
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-27 10:38:46 +0600

    buddy

--- buddy/buddies.js
+++ buddy/buddies.js
@@ -9,6 +9,10 @@ const displayBuddies = data => {
     const buddies = data.results;
     const buddiesDiv = document.getElementById('buddies');
     for (const buddy of buddies) {
-        console.log(buddy.email);
+        console.log(buddy.name.first)
+        const p = document.createElement('p');
+        p.innerText = `Name: ${buddy.name.title} ${buddy.name.first} ${buddy.name.last},
+        Email : ${buddy.email}`
+        buddiesDiv.appendChild(p);
     }
 }
